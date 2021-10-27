6a8681dcdf92e5f7551fe8c452f2d80f43a83d2c
EmptyBox12 <ataberktumay@gmail.com>
2021-10-28 00:29:09 +0300

    simple crud process

---
+++ ataberk-tumay-week1/crud/crud.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+
+fs.mkdir("odev", { recursive: true }, (err) => {
+  if (err) throw err;
+});
+
+
+fs.appendFile('./odev/employees.json', '{"name": "Employee 1 Name", "salary": 2000}', (err) => {
+  if (err) throw err;
+  console.log('The "data to append" was appended to file!');
+});
+
+fs.readFile('./odev/employees.json', (err, data) => {
+  if (err) throw err;
+  console.log((JSON.parse(data)));
+  console.log("read");
+});
+
+
+
+fs.readFile('./odev/employees.json', (err, data) => {
+  if (err) throw err;
+  let dataRaw = {"name": "Employee 1 Name", "salary": 5000};
+  let newData = JSON.stringify(dataRaw);
+  fs.writeFile('./odev/employees.json', newData, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  });
+  fs.unlink('odev/employees.json', (err) => {
+    if (err) throw err;
+    console.log('path/file.txt was deleted');
+  });
+});
+
