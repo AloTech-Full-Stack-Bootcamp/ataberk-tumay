54aca9ea9393f922e87dd9d8d07ab3b48f55e65b
EmptyBox12 <ataberktumay@gmail.com>
2021-11-04 01:55:43 +0300

    added update and delete

--- week2/cleanBlog/app.js
+++ week2/cleanBlog/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 let ejs = require("ejs");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const Post = require("./model/Post.js");
 //%
 
@@ -18,6 +19,11 @@ mongoose.connect("mongodb://localhost/cleanblog-db", {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"],
+  })
+);
 
 //ROUTES
 app.get("/", async (req, res) => {
@@ -32,14 +38,39 @@ app.get("/about", (req, res) => {
 app.get("/add_post", (req, res) => {
   res.render("add_post");
 });
-app.get("/posts/:id", async (req,res)=>{
+app.get("/posts/:id", async (req, res) => {
   let id = req.params.id;
   let post = await Post.findById(id);
   res.render("post", {
-    post
+    post,
   });
 });
-
+app.get("/posts/edit/:id", async (req, res) => {
+  let id = req.params.id;
+  let post = await Post.findById(id);
+  res.render("edit", {
+    post,
+  });
+});
+app.put("/posts/:id", async (req, res) => {
+  let id = req.params.id;
+  let post = Post.findById(id);
+  await Post.findByIdAndUpdate(
+    id,
+    {
+      title: req.body.title || post.title,
+      detail: req.body.detail || post.detail,
+    },
+    {
+      new: true,
+    }
+  );
+  res.redirect(`/posts/${id}`);
+});
+app.delete("/posts/:id", async (req, res) => {
+  await Post.findOneAndDelete({ _id: req.params.id });
+  res.redirect("/");
+});
 app.post("/posts", async (req, res) => {
   await Post.create({
     title: req.body.title,
