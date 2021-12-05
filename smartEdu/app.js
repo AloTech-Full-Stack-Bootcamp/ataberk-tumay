10d145cefa004463a038d8a338f99477750c6365
EmptyBox12 <ataberktumay@gmail.com>
2021-12-06 01:36:21 +0300

    create partials

--- smartEdu/app.js
+++ smartEdu/app.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
 const PORT = "3000";
+//%
+
+app.set("view engine", "ejs");
+//Middlewares
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("Test");
+  res.render("index", {
+    pageName: "index",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    pageName: "about",
+  });
 });
 
 app.listen(PORT, () => {
