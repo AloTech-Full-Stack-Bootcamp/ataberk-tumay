a4ccc554287918fa21cc299390ed09d0043fb199
EmptyBox12 <ataberktumay@gmail.com>
2021-12-07 00:17:32 +0300

    simplify first assignment

--- week6/pythonProject/main.py
+++ week6/pythonProject/main.py
@@ -3,25 +3,14 @@ import random
 
 def random_number_generator_while(n, l):
     numberList = []
-    counter = 0
     while len(numberList) < n:
-        if counter == 0:
-            number = random.randrange(10 ** (l - 1), 10 ** l - 1)
-            counter += 1
-        else:
-            number = random.randrange(10 ** (l - 1), 10 ** l - 1)
-            counter += 1
-
-        if number in numberList:
-            counter = 0
-        else:
+        number = random.randrange(10 ** (l - 1), 10 ** l - 1)
+        if number not in numberList:
             numberList.append(number)
-            counter = 0
-
     print(numberList)
 
 
-random_number_generator_while(10, 3)
+random_number_generator_while(21, 3)
 
 
 # second assignment. increase input by 1 and reverse the return value
