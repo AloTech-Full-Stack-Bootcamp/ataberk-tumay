dfdafbef9a0299934507657cf5932916808da025
EmptyBox12 <ataberktumay@gmail.com>
2021-12-10 22:11:16 +0300

    create custom middlewares(redirect, auth)

--- smartEdu/controllers/authController.js
+++ smartEdu/controllers/authController.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcrypt");
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
-    res.status(201).redirect("/login"); 
+    res.status(201).redirect("/login");
   } catch (error) {
     res.status(400).json({
       status: "fail",
@@ -17,18 +17,36 @@ exports.loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email: email });
-    if(user){
-      bcrypt.compare(password, user.password, function(err, result){
-        if(result){
+    if (user) {
+      bcrypt.compare(password, user.password, function (err, result) {
+        if (result) {
           req.session.userID = user._id;
-          res.status(200).redirect("/");
+          res.status(200).redirect("/users/dashboard");
         }
-      })
-    } 
+      });
+    }
   } catch (error) {
     res.status(400).json({
       status: "fail",
       error,
     });
   }
+};
+
+exports.logoutUser = (req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/");
+  });
+};
+
+exports.getDashboardPage = async (req, res) => {
+  try {
+    let id = req.session.userID;
+    let user = await User.findById(id);
+    console.log(user);
+    res.render("dashboard", {
+      pageName: "dashboard",
+      user: user,
+    });
+  } catch (error) {}
 };
